525e9171b3c52db366cf537872201dea5f5a0936
alfonsus20 <alfonsusavianto@student.ub.ac.id>
2021-11-21 18:17:18 +0700

    add bottom navigation and fragments

--- app/src/main/java/com/example/teenage/MainActivity.kt
+++ app/src/main/java/com/example/teenage/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.teenage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        replaceFragment(DrinkFragment())
+
         supportActionBar?.hide()
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_drink -> replaceFragment(DrinkFragment())
+                R.id.nav_schedule -> replaceFragment(HistoryFragment())
+                R.id.nav_notification -> replaceFragment(NotificationFragment())
+                R.id.nav_user -> replaceFragment(UserFragment())
+            }
+            true
+        }
     }
+
+    private fun replaceFragment(fragment : Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
+    }
+
 }

f9d5b38475b657a774c2688ab3efcd4bddb11b35
Aristo Renaldo <aristo1renaldo@gmail.com>
2021-11-23 18:47:04 +0800

    menambah timepicker di Account.kt

--- app/src/main/java/com/example/teenage/Account.kt
+++ app/src/main/java/com/example/teenage/Account.kt
@@ -1,11 +1,40 @@
 package com.example.teenage
 
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class Account : AppCompatActivity() {
+    lateinit var wakeTimeText: TextView
+    lateinit var sleepTimeText: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
+
+        wakeTimeText = findViewById(R.id.wakeTimeText)
+        sleepTimeText = findViewById(R.id.sleepTimeText)
+
+        wakeTimeText.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { _, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+                wakeTimeText.text = SimpleDateFormat("HH:mm").format(cal.time)
+            }
+            TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+        }
+        sleepTimeText.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { _, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+                sleepTimeText.text = SimpleDateFormat("HH:mm").format(cal.time)
+            }
+            TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+        }
     }
 }

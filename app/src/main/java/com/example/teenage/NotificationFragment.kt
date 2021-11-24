f19c0ddc7392a4a46a22ce72aa0fd7ddfa303e40
alfonsus20 <alfonsusavianto@student.ub.ac.id>
2021-11-25 00:50:42 +0700

    action bar fixing and finish menu page

--- app/src/main/java/com/example/teenage/NotificationFragment.kt
+++ app/src/main/java/com/example/teenage/NotificationFragment.kt
@@ -1,22 +1,37 @@
 package com.example.teenage
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
 class NotificationFragment : Fragment() {
+    @SuppressLint("RestrictedApi")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_notification, container, false)
-        val title: TextView = view.findViewById(R.id.TitleBar)
-        title.setText("Notification")
         (activity as MainActivity).bottomNav.menu.getItem(2).isChecked = true
         return view
+    }
+
+    @SuppressLint("RestrictedApi")
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity).supportActionBar?.setShowHideAnimationEnabled(false)
+        (activity as AppCompatActivity).supportActionBar?.setTitle(Html.fromHtml("<font color='#14279B'>Notification</font>"))
+        (activity as AppCompatActivity).supportActionBar?.show()
+    }
+
+    override fun onStop() {
+        (activity as AppCompatActivity).supportActionBar?.hide()
+        super.onStop()
     }
 }
--- app/src/main/java/com/example/teenage/UserFragment.kt
+++ app/src/main/java/com/example/teenage/UserFragment.kt
@@ -1,14 +1,19 @@
 package com.example.teenage
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.Html
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
 class UserFragment : Fragment() {
+    @SuppressLint("RestrictedApi")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -16,16 +21,26 @@ class UserFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_user, container, false)
 
-        val title: TextView = view.findViewById(R.id.TitleBar)
-        title.setText("User")
-        val accountTextView:TextView = view.findViewById(R.id.account)
+        val accountTextView: TextView = view.findViewById(R.id.account)
         accountTextView.setOnClickListener {
-            val intent = Intent (activity,Account::class.java)
+            val intent = Intent(activity, Account::class.java)
             startActivity(intent)
         }
 
-//        (activity as AppCompatActivity).supportActionBar?.hide()
         (activity as MainActivity).bottomNav.menu.getItem(3).isChecked = true
         return view
+    }
+
+    @SuppressLint("RestrictedApi")
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity).supportActionBar?.setShowHideAnimationEnabled(false)
+        (activity as AppCompatActivity).supportActionBar?.setTitle(Html.fromHtml("<font color='#14279B'>User</font>"))
+        (activity as AppCompatActivity).supportActionBar?.show()
+    }
+
+    override fun onStop() {
+        (activity as AppCompatActivity).supportActionBar?.hide()
+        super.onStop()
     }
 }

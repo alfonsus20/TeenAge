9f94f56d3d680a90a9a6d10416526b6dab35ea79
Aristo Renaldo <aristo1renaldo@gmail.com>
2021-11-25 12:55:53 +0800

    Mengubah dan memperbaiki activity Account.kt

--- app/src/main/java/com/example/teenage/Account.kt
+++ app/src/main/java/com/example/teenage/Account.kt
@@ -1,40 +1,37 @@
 package com.example.teenage
 
+import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
-import java.text.SimpleDateFormat
+import androidx.annotation.RequiresApi
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 class Account : AppCompatActivity() {
-    lateinit var wakeTimeText: TextView
-    lateinit var sleepTimeText: TextView
+
+    lateinit var birthDateText: TextView
+    var cal = Calendar.getInstance()
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
+        var formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy")
+        val year = cal.get(Calendar.YEAR)
+        val month = cal.get(Calendar.MONTH)
+        val day = cal.get(Calendar.DAY_OF_MONTH)
+        birthDateText = this.findViewById(R.id.birthDateText)
 
-        wakeTimeText = findViewById(R.id.wakeTimeText)
-        sleepTimeText = findViewById(R.id.sleepTimeText)
-
-        wakeTimeText.setOnClickListener {
-            val cal = Calendar.getInstance()
-            val timeSetListener = TimePickerDialog.OnTimeSetListener { _, hour, minute ->
-                cal.set(Calendar.HOUR_OF_DAY, hour)
-                cal.set(Calendar.MINUTE, minute)
-                wakeTimeText.text = SimpleDateFormat("HH:mm").format(cal.time)
-            }
-            TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
-        }
-        sleepTimeText.setOnClickListener {
-            val cal = Calendar.getInstance()
-            val timeSetListener = TimePickerDialog.OnTimeSetListener { _, hour, minute ->
-                cal.set(Calendar.HOUR_OF_DAY, hour)
-                cal.set(Calendar.MINUTE, minute)
-                sleepTimeText.text = SimpleDateFormat("HH:mm").format(cal.time)
-            }
-            TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+        birthDateText.setOnClickListener {
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, mYear, mMonth, mDay ->
+                var date = LocalDate.of(mYear, mMonth, mDay).format(formatter)
+                birthDateText.setText(date.toString())
+            },year, month, day )
+            dpd.show()
         }
     }
 }
